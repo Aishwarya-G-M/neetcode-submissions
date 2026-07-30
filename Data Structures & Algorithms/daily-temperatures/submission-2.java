4c2b4eb326cfd33e08bece7827b9464d9ae25df6
Aishwarya Govindappa Mahdev <aishwarya.govindappamahadev@gmail.com>
2026-07-30 12:10:09 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] result = new int[len];
+        int pos = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<len;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){       
+                pos = stack.pop(); 
+                result[pos] = i-pos;  
+            } 
+            stack.push(i);
+        }
+        return result;
+    }
+}
